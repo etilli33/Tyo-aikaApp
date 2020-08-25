4edc1f553b676f18ccc1b18e516760c4992eea34
Till Paala <till.paala@pori.fi>
2020-08-25 14:13:54 +0300

    Add a share button to share your logging history

--- app.js
+++ app.js
@@ -322,7 +322,9 @@ let UIController = (function() {
     workingTimeInput: '#working-time-input',
     workingTimePercent: '#working-time-percent',
     workingTimeSaldo: '#starting-saldo',
-    settingsSubmit: '#settings-submit'
+    settingsSubmit: '#settings-submit',
+    historyTable: '.history',
+    shareButton: '.share-button'
   };
   let saveSettings = function() {
     //save changes to working time
@@ -395,6 +397,36 @@ return {
       table.classList.remove('hidden');
       }
     },
+
+    webShare: async function() {
+      if (navigator.share === undefined) {
+        logError('Error: Unsupported feature: navigator.share()');
+        return;
+      }
+  
+      const title_input = 'Työajanseuranta';
+      const text_input = document.querySelector(DOMStrings.historyTable);
+  
+  
+      const title = title_input.disabled ? undefined : title_input;
+      const text = text_input.disabled ? undefined : text_input;
+      //const url = url_input.disabled ? undefined : url_input.value;
+      //const files = file_input.disabled ? undefined : file_input.files;
+  
+      if (files && files.length > 0) {
+        if (!navigator.canShare || !navigator.canShare({files})) {
+          logError('Error: Unsupported feature: navigator.canShare()');
+          return;
+        }
+      }
+  
+      try {
+        await navigator.share({title, text});
+        logText('Successfully sent share');
+      } catch (error) {
+        logError('Error sharing: ' + error);
+      }
+    },
     setModal: function() {
       // Get the modal
       let modal = document.querySelector(DOMStrings.modal);
@@ -454,12 +486,7 @@ let Controller = (function(AppController, UIController) {
 
     document.querySelector(DOM.buttonOAOUT).addEventListener('click', ctrAddOwnOut);
 
-    /*
-    document.addEventListener('keypress', function(event) {
-      if (event.keyCode === 13 || event.which === 13) {//for older browsers the second version
-        ctrlAddItem();
-      };
-      */
+
       //click for saving settings
       document.querySelector(DOM.settingsSubmit).addEventListener('click', UIController.updateSettings);
 
@@ -468,7 +495,32 @@ let Controller = (function(AppController, UIController) {
 
       document.querySelector(DOM.workingTimeInput).addEventListener('blur', UIController.updatePercent);
 
+      //Share button
+      document.querySelector(DOM.shareButton).addEventListener('click', UIController.webShare);
+
+
+      if (navigator.share === undefined) {
+        if (window.location.protocol === 'http:') {
+          // navigator.share() is only available in secure contexts.
+          window.location.replace(window.location.href.replace(/^http:/, 'https:'));
+        } else {
+          logError('Error: You need to use a browser that supports this draft ' +
+                   'proposal.');
+        }
+      }
     };
+
+    //error function for webshare function
+    function logText(message, isError) {
+      if (isError)
+        console.error(message);
+      else
+        console.log(message);
+    }
+
+    function logError(message) {
+      logText(message, true);
+    }
 
     let ctrAddIn = function() {
       //call function from UIController
@@ -522,9 +574,15 @@ let Controller = (function(AppController, UIController) {
   };
 
   let autoLogOut = function() {
-    //log out after 20:15
+    //TODO: log out after 20:15
 
-  }
+  };
+
+
+
+
+
+
 
   return {
     init: function() {
